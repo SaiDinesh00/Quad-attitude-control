63dc8a43768f1d34f2999fe14efd4af4ef36c253
SaiDinesh <22m2012@iitb.ac.in>
2024-02-14 12:00:20 +0530

    added pid

--- control.m
+++ control.m
@@ -4,7 +4,7 @@ function u = control(t,X, X_des)
     %% Simple PID Controller
     %PD controller
     kp_rpy = [500; 500; 3000];
-    kd_rpy = [300; 300; 2000];
+    kd_rpy = [300; 300; 3000];
     %current errors
     ang_error = X_des(1:3) - X(1:3);
     ang_rate_error = X_des(4:6) - X(4:6);    
--- main.m
+++ main.m
@@ -7,10 +7,10 @@ length = 0.225; % Arm Length (m)
 I = diag([4.856e-3, 4.856e-3, 8.801e-3]); % Inertia matrix
 k_t = 2.980e-6; % Thrust coefficient
 k_d = 1.140e-7; % drag coefficient
-g = 9.81;
+g = 9.81; % gravity
 params = containers.Map({'mass','length', 'Inertia', 'k_t', 'k_d', 'g'},{mass, length, I, k_t, k_d, g});
-d2r = pi/180;
-r2d = 180/pi;
+d2r = pi/180; %degree to radians conversion
+r2d = 180/pi; %radians to degree conversion
 %% Define Simulation time
 Ns = 2000;
 t_end = 20;
